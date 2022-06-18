d2ccdb64059a898158d5d60fc479fe1d07d6ca76
SnegurkaWs <snegurka@web-esse.ru>
2022-06-18 12:12:35 +0500

    ADD: comments component

---
+++ src/components/Comments/Comments.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrapper } from "./Comments.styles";
+
+const Comments: React.FC = () => {
+
+    return (
+        <Wrapper>
+            <h2>Comments</h2>
+        </Wrapper>
+    )
+}
+
+export default Comments
--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -12,28 +12,30 @@ import Gallery from '../Gallery/Gallery';
 import ActorsList from '../ActorsList/ActorsList';
 
 import { Wrapper } from './Movie.styles';
+import Comments from '../Comments/Comments';
 
 
 
 const Movie: React.FC = () => {
-    const { movieId } = useParams<{movieId: string}>();
+    const { movieId } = useParams<{ movieId: string }>();
     const { state: movie, loading, error } = useMovieFetch(movieId);
 
     if (loading) return <Spinner />;
     if (error) return <Error />
-   
+
 
     return (
         <Wrapper>
-        <BreadCrumb movieTitle={movie.original_title} />
+            <BreadCrumb movieTitle={movie.original_title} />
 
-        <MovieInfo movie={movie} />
+            <MovieInfo movie={movie} />
 
-        {movie.runtime ? (<MovieInfoBar time={movie.runtime} buget={movie.budget} revenue={movie.revenue}/>) : null }
+            {movie.runtime ? (<MovieInfoBar time={movie.runtime} buget={movie.budget} revenue={movie.revenue} />) : null}
 
-        {movie.backdrops ? (<Gallery header='Photos' photos={movie.backdrops}/>) : null}
+            {movie.backdrops ? (<Gallery header='Photos' photos={movie.backdrops} />) : null}
 
-        {movie.actors ? (<ActorsList actors={movie.actors} />) : null}
+            {movie.actors ? (<ActorsList actors={movie.actors} />) : null}
+            <Comments />
         </Wrapper>
     )
 }
