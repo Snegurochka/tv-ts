df50a5926c0477dcd494a3dc6c6a69ff23b1d4fe
SnegurkaWs <snegurka@web-esse.ru>
2022-07-04 10:18:09 +0500

    ADD: Saga Pattern with a detached task

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,13 +1,20 @@
-import { all, call } from 'redux-saga/effects';
+import { spawn } from 'typed-redux-saga';
 
 import { commentsSaga } from './comments/comments.saga';
 import { movieSaga } from './movie/movie.saga';
 import { userSaga } from './user/user.saga';
 
+// export function* rootSaga() {
+//     yield all([
+//         call(commentsSaga),
+//         call(userSaga),
+//         call(movieSaga)
+//     ]);
+// }
+
+// Saga Pattern with a detached task.
 export function* rootSaga() {
-    yield all([
-        call(commentsSaga),
-        call(userSaga),
-        call(movieSaga)
-    ]);
+    yield spawn(userSaga);
+    yield spawn(commentsSaga);
+    yield spawn(movieSaga);
 }
