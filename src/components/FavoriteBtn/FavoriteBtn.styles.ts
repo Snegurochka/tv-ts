aecba48132ae29b727029fcda01ee0079352a0cc
SnegurkaWs <snegurka@web-esse.ru>
2022-07-06 15:01:32 +0500

    ADD: FavoriteBtn component

---
+++ src/components/FavoriteBtn/FavoriteBtn.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FavoriteBtnWrapper = styled.div`
+    position  : absolute;
+    transition: all 0.3s ease;
+    cursor    : pointer;
+
+    svg {
+        width: 30px;
+    }
+`;
